27fe35da29d3c617a07393a704cebd6bbeaabbb3
hkoflax <hkoflax@users.noreply.github.com>
2019-05-25 03:05:33 -0400

    Complete Entries Controller for the API

--- Treehouse.FitnessFrog.Spa/App_Start/WebApiConfig.cs
+++ Treehouse.FitnessFrog.Spa/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,14 @@ namespace Treehouse.FitnessFrog.Spa
     {
         public static void Register(HttpConfiguration config)
         {
+            var jsonSerialiserSettings = config.Formatters.JsonFormatter.SerializerSettings;
+            jsonSerialiserSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            jsonSerialiserSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Routes.MapHttpRoute(
+                name:"DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id= RouteParameter.Optional});
         }
     }
 }
